a4de4d1bc261fc57fb0b1255dceac8012452bbb9
成龙 <562522970@qq.com>
2014-09-23 17:19:53 +0800

    OpenGL
    add OpenGL

---
+++ CH1/CH1/example1_2.cpp
@@ -0,0 +1,37 @@
+#include "stdafx.h"
+#define ENABLE 0
+#if ENABLE
+void init()
+{
+	glClearColor(0.0,0.0,0.0,0.0);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(0.0,1.0,0.0,1.0,-1.0,1.0);
+}
+
+void display(void)
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+	glColor3f(1.0,1.0,1.0);
+	glBegin(GL_POLYGON);
+		glVertex3f(0.25,0.25,0.0);
+		glVertex3f(0.25,0.75,0.0);
+		glVertex3f(0.75,0.75,0.0);
+		glVertex3f(0.75,0.25,0.0);
+	glEnd();
+	glFlush();
+}
+
+int main(int argc, char *argv[])
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE);
+	glutInitWindowPosition(220, 200);
+	glutInitWindowSize(800, 800);
+	glutCreateWindow("hello");
+	init();
+	glutDisplayFunc(&display);
+	glutMainLoop();
+	return 0;
+}
+#endif
